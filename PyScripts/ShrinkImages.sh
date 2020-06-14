18333cacf1034da28e8782029b1a78437a594f9f
darrellanderson <darrell.anderson@gmail.com>
2020-06-14 12:42:03 -0700

    use 'best' for intial png -> jpg conversions

--- PyScripts/ShrinkImages.sh
+++ PyScripts/ShrinkImages.sh
@@ -4,6 +4,6 @@ while read -r line
 do
     filename=$( printf "%s" "$line" | tr -cs 'a-zA-Z0-9' '_' )
     src="/Users/darrell/t.images/$filename"
-    dst="/Users/darrell/t.images2/$filename"
-    sips -s format jpeg -s formatOptions 80 "$src" --out "$dst"
+    dst="/Users/darrell/t.images_jpg_best/$filename"
+    sips -s format jpeg -s formatOptions best "$src" --out "$dst"
 done

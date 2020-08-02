24349e3a0112e3c6f478f5617e019eb84f640fd6
darrellanderson <darrell.anderson@gmail.com>
2020-08-01 19:33:47 -0700

    action card positioning, still need to do other types

--- Kruft/ti4-card-images/main.py
+++ Kruft/ti4-card-images/main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+
+# dev_appserver.py app.yaml
 # gcloud app deploy app.yaml --project ti4-card-images
 """
 """
@@ -15,7 +20,7 @@ import os
 import os.path
 import webapp2
 
-PARAGRAPH_LINE_HEIGHT_SCALE = 1.5
+PARAGRAPH_LINE_HEIGHT_SCALE = 1.4
 
 # -----------------------------------------------------------------------------
 
@@ -43,20 +48,20 @@ def imageToJPEG(image):
     os.remove(file)
     return jpg
 
-def nudgeY(font, text, maxWidth, y1, y2):
+def nudgeY(font, text, maxX, y1, y2):
     (w, h) = font.getsize(text)
-    return y1 if w <= maxWidth else y2
+    return y1 if w <= maxX else y2
 
 # -----------------------------------------------------------------------------
 
-def wrapText(draw, font, text, x, y, maxWidth, fill, lineH):
+def wrapText(draw, font, text, x, y, maxX, fill, lineH):
     words = filter(None, text.split(' '))
     text = ''
     lineWidth = 0
     (spaceW, h) = font.getsize(' ')
     for word in words:
         (w, h) = font.getsize(word)
-        if lineWidth + w > maxWidth:
+        if lineWidth + w > maxX:
             draw.text((x, y), text, font=font, fill=fill)
             text = ''
             lineWidth = 0
@@ -66,14 +71,14 @@ def wrapText(draw, font, text, x, y, maxWidth, fill, lineH):
     draw.text((x, y), text, font=font, fill=fill)
     return y + (lineH * PARAGRAPH_LINE_HEIGHT_SCALE)
 
-def wrapTextCenter(draw, font, text, x, y, maxWidth, fill, lineH):
+def wrapTextCenter(draw, font, text, x, y, maxX, fill, lineH):
     words = filter(None, text.split(' '))
     text = ''
     lineWidth = 0
     (spaceW, h) = font.getsize(' ')
     for word in words:
         (w, h) = font.getsize(word)
-        if lineWidth + w > maxWidth:
+        if lineWidth + w > maxX:
             x2 = x - (lineWidth / 2)
             draw.text((x2, y), text, font=font, fill=fill)
             text = ''
@@ -85,7 +90,7 @@ def wrapTextCenter(draw, font, text, x, y, maxWidth, fill, lineH):
     draw.text((x2, y), text, font=font, fill=fill)
     return y + (lineH * PARAGRAPH_LINE_HEIGHT_SCALE)
 
-def wrapTextCenterHV(draw, font, text, x, y, maxWidth, fill, lineH):
+def wrapTextCenterHV(draw, font, text, x, y, maxX, fill, lineH):
     text = text.replace('\n', ' \n ')
     lines = []
     (spaceW, h) = font.getsize(' ')
@@ -94,7 +99,7 @@ def wrapTextCenterHV(draw, font, text, x, y, maxWidth, fill, lineH):
     lineWidth = 0
     for word in words:
         (w, h) = font.getsize(word)
-        if word == '\n' or lineWidth + w > maxWidth:
+        if word == '\n' or lineWidth + w > maxX:
             lines.append(text)
             text = ''
             lineWidth = 0
@@ -112,7 +117,7 @@ def wrapTextCenterHV(draw, font, text, x, y, maxWidth, fill, lineH):
         y += lineH
     return y + (lineH * PARAGRAPH_LINE_HEIGHT_SCALE)
 
-def wrapTextBoldStart(draw, font1, font2, text, x, y, maxWidth, fill, lineH):
+def wrapTextBoldStart(draw, font1, font2, text, x, y, maxX, fill, lineH):
     font = font1
     (spaceW, h) = font.getsize(' ')
     words = filter(None, text.split(' '))
@@ -121,7 +126,7 @@ def wrapTextBoldStart(draw, font1, font2, text, x, y, maxWidth, fill, lineH):
     indent = 0
     for word in words:
         (w, h) = font.getsize(word)
-        if x + w > maxWidth:
+        if x + w > maxX:
             x = startX + indent
             y += lineH
         draw.text((x, y), word, font=font, fill=fill)
@@ -138,6 +143,30 @@ def wrapTextBoldStart(draw, font1, font2, text, x, y, maxWidth, fill, lineH):
 
 # -----------------------------------------------------------------------------
 
+CARD_W = 500
+CARD_H = 750
+
+ACTION_TITLE_L = 85
+ACTION_TITLE_R = 100
+ACTION_TITLE_Y1 = 96
+ACTION_TITLE_Y2 = 76
+ACTION_TITLE_TEXT_SIZE = 39
+ACTION_TITLE_TEXT_H = 39
+
+ACTION_BODY_L = 70
+ACTION_BODY_R = 40
+ACTION_BODY_Y = 206
+ACTION_BODY_TEXT_SIZE = 31
+ACTION_BODY_TEXT_H = 37
+
+ACTION_FLAVOR_L = 275
+ACTION_FLAVOR_R = 100
+ACTION_FLAVOR_LINE_Y = 559
+ACTION_FLAVOR_DY = 14
+ACTION_FLAVOR_TEXT_SIZE = 27
+ACTION_FLAVOR_TEXT_H = 31
+
+# REPLACE THESE WITH PER CARD CONSTANTS
 TITLE_SIZE = 44
 TITLE_LINEH = 44
 TYPE_SIZE = 32
@@ -147,41 +176,51 @@ BODY_SM_LINEH = 38
 BODY_LG_SIZE = 40
 BODY_LG_LINEH = 47
 
-def actionCard(title, body, flavor):
-    img = getImage('ActionCard.jpg')
+def actionCard(title, body, flavor, isCodex):
+    img = getImage('ActionCardCodex.jpg' if isCodex else 'ActionCard.jpg')
     draw = ImageDraw.Draw(img)
 
-    font = getFont('HandelGothicDBold.otf', TITLE_SIZE)
+    font = getFont('HandelGothicDBold.otf', ACTION_TITLE_TEXT_SIZE)
     color = (255, 232, 150, 255)
     text = title
-    x = 85
-    y1 = 92
-    y2 = 72
-    maxWidth = 400
-    lineH = TITLE_LINEH
-    y = nudgeY(font, text, maxWidth, y1, y2)
-    wrapText(draw, font, text, x, y, maxWidth, color, lineH)
+    x = ACTION_TITLE_L
+    y1 = ACTION_TITLE_Y1
+    y2 = ACTION_TITLE_Y2
+    maxX = CARD_W - ACTION_TITLE_R
+    lineH = ACTION_TITLE_TEXT_H
+    y = nudgeY(font, text, maxX, y1, y2)
+    wrapText(draw, font, text, x, y, maxX, color, lineH)
 
-    font1 = getFont('MyriadProBold.ttf', BODY_SM_SIZE)
-    font2 = getFont('MyriadProSemibold.otf', BODY_SM_SIZE)
+    font1 = getFont('MyriadProBold.ttf', ACTION_BODY_TEXT_SIZE)
+    font2 = getFont('MyriadProSemibold.otf', ACTION_BODY_TEXT_SIZE)
     color = (255, 255, 255, 255)
     text = body
-    x = 60
-    y = 200
-    maxWidth = 480
-    lineH = BODY_SM_LINEH
+    x = ACTION_BODY_L
+    y = ACTION_BODY_Y
+    maxX = CARD_W - ACTION_BODY_R
+    lineH = ACTION_BODY_TEXT_H
     for line in text.split('\n'):
-        y = wrapTextBoldStart(draw, font1, font2, line, x, y, maxWidth, color, lineH)
+        y = wrapTextBoldStart(draw, font1, font2, line, x, y, maxX, color, lineH)
 
-    font = getFont('MyriadWebProItalic.ttf', 26)
+    y = max(y, ACTION_FLAVOR_LINE_Y)
+    draw.line([(107, y), (CARD_W - 69, y)], fill=(255, 255, 255, 255), width=3)
+
+
+    font = getFont('MyriadWebProItalic.ttf', ACTION_FLAVOR_TEXT_SIZE)
     color = (255, 255, 255, 255)
     text = flavor
-    x = 270
-    y = max(y, 575)
-    maxWidth = 425
-    lineH = 31
+    x = ACTION_FLAVOR_L
+    y = y + ACTION_FLAVOR_DY
+    maxX = CARD_W - ACTION_FLAVOR_R
+    lineH = ACTION_FLAVOR_TEXT_H
     for line in text.split('\n'):
-        y = wrapTextCenter(draw, font, line, x, y, maxWidth, color, lineH)
+        y = wrapTextCenter(draw, font, line, x, y, maxX, color, lineH)
+
+    #img2 = getImage('Bribery.jpg')
+    #img.putalpha(1)
+    #img2.putalpha(1)
+    #img = Image.blend(img, img2, 0.4)
+    #img = img.convert('RGB')
 
     return imageToJPEG(img)
 
@@ -195,10 +234,10 @@ def secretObjectiveCard(title, type, body):
     x = 250
     y1 = 35
     y2 = 15
-    maxWidth = 400
+    maxX = 400
     lineH = TITLE_LINEH
-    y = nudgeY(font, text, maxWidth, y1, y2)
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    y = nudgeY(font, text, maxX, y1, y2)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     x = 125
     y = 130
@@ -212,18 +251,18 @@ def secretObjectiveCard(title, type, body):
     text = type
     x = 250
     y = 127
-    maxWidth = 400
+    maxX = 400
     lineH = 26
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     font = getFont('MyriadProSemibold.otf', BODY_LG_SIZE)
     color = (255, 255, 255, 255)
     text = body
     x = 250
     y = 395
-    maxWidth = 450
+    maxX = 450
     lineH = BODY_LG_LINEH
-    y = wrapTextCenterHV(draw, font, text, x, y, maxWidth, color, lineH)
+    y = wrapTextCenterHV(draw, font, text, x, y, maxX, color, lineH)
 
     return imageToJPEG(img)
 
@@ -238,19 +277,19 @@ def publicObjectiveCard(level, title, body):
     x = 250
     y1 = 35
     y2 = 15
-    maxWidth = 400
+    maxX = 400
     lineH = TITLE_LINEH
-    y = nudgeY(font, text, maxWidth, y1, y2)
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    y = nudgeY(font, text, maxX, y1, y2)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     font = getFont('MyriadProSemibold.otf', BODY_LG_SIZE)
     color = (255, 255, 255, 255)
     text = body
     x = 250
     y = 395
-    maxWidth = 450
+    maxX = 450
     lineH = BODY_LG_LINEH
-    y = wrapTextCenterHV(draw, font, text, x, y, maxWidth, color, lineH)
+    y = wrapTextCenterHV(draw, font, text, x, y, maxX, color, lineH)
 
     return imageToJPEG(img)
 
@@ -264,10 +303,10 @@ def agendaCard(title, type, body):
     x = 250
     y1 = 35
     y2 = 15
-    maxWidth = 400
+    maxX = 400
     lineH = TITLE_LINEH
-    y = nudgeY(font, text, maxWidth, y1, y2)
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    y = nudgeY(font, text, maxX, y1, y2)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     x = 200
     y = 160
@@ -281,9 +320,9 @@ def agendaCard(title, type, body):
     text = type
     x = 250
     y = 165
-    maxWidth = 400
+    maxX = 400
     lineH = TYPE_LINEH
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     font1 = getFont('MyriadProBold.ttf', BODY_SM_SIZE)
     font2 = getFont('MyriadProSemibold.otf', BODY_SM_SIZE)
@@ -293,20 +332,20 @@ def agendaCard(title, type, body):
     lineH = BODY_SM_LINEH
     if 'Elect ' in text:
         x = 250
-        maxWidth = 400
+        maxX = 400
         for line in text.split('\n'):
             if line.startswith('Elect '):
-                y = wrapTextCenter(draw, font1, line, x, y, maxWidth, color, lineH)
+                y = wrapTextCenter(draw, font1, line, x, y, maxX, color, lineH)
             else:
-                y = wrapTextCenter(draw, font2, line, x, y, maxWidth, color, lineH)
+                y = wrapTextCenter(draw, font2, line, x, y, maxX, color, lineH)
     else:
         x = 60
-        maxWidth = 430
+        maxX = 430
         for line in text.split('\n'):
             if line.startswith('For:') or line.startswith('Against:'):
-                y = wrapTextBoldStart(draw, font1, font2, line, x, y, maxWidth, color, lineH)
+                y = wrapTextBoldStart(draw, font1, font2, line, x, y, maxX, color, lineH)
             else:
-                y = wrapText(draw, font2, line, x, y, maxWidth, color, lineH)
+                y = wrapText(draw, font2, line, x, y, maxX, color, lineH)
 
     return imageToJPEG(img)
 
@@ -321,46 +360,46 @@ def nobilityCard(color, title, type, body, footer, points):
     x = 250
     y1 = 35
     y2 = 15
-    maxWidth = 400
+    maxX = 400
     lineH = TITLE_LINEH
-    y = nudgeY(font, text, maxWidth, y1, y2)
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    y = nudgeY(font, text, maxX, y1, y2)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     font = getFont('MyriadProBold.ttf', TYPE_SIZE)
     color = (255, 255, 255, 255) if type.lower() == 'status phase' else (255, 0, 0, 255)
     text = type
     x = 250
     y = 135
-    maxWidth = 400
+    maxX = 400
     lineH = TYPE_LINEH
-    wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     font = getFont('MyriadProSemibold.otf', BODY_LG_SIZE)
     color = (255, 255, 255, 255)
     text = body
     x = 250
     y = 375
-    maxWidth = 450
+    maxX = 450
     lineH = BODY_LG_LINEH
-    y = wrapTextCenterHV(draw, font, text, x, y, maxWidth, color, lineH)
+    y = wrapTextCenterHV(draw, font, text, x, y, maxX, color, lineH)
 
     font = getFont('MyriadProBold.ttf', 40)
     color = (255, 255, 255, 255) if footer.lower() == 'public' else (255, 0, 0, 255)
     text = footer
     x = 250
     y = 520
-    maxWidth = 450
+    maxX = 450
     lineH = 40
-    y = wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    y = wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     font = getFont('HandelGothicDBold.otf', 130)
     color = (255, 255, 255, 255)
     text = str(points)
     x = 265
     y = 565
-    maxWidth = 450
+    maxX = 450
     lineH = 130
-    y = wrapTextCenter(draw, font, text, x, y, maxWidth, color, lineH)
+    y = wrapTextCenter(draw, font, text, x, y, maxX, color, lineH)
 
     return imageToJPEG(img)
 
@@ -381,14 +420,14 @@ class CardHandler(webapp2.RequestHandler):
         logging.info('Card "' + card + '" title="' + title + '" type="' + type + '" body="' + body + '" flavor="' + flavor + '"')
 
         hash = hashlib.sha256()
-        hash.update(card)
-        hash.update(title)
-        hash.update(type)
-        hash.update(body)
-        hash.update(footer)
-        hash.update(flavor)
-        hash.update(color)
-        hash.update(points)
+        hash.update(card.encode('utf-8'))
+        hash.update(title.encode('utf-8'))
+        hash.update(type.encode('utf-8'))
+        hash.update(body.encode('utf-8'))
+        hash.update(footer.encode('utf-8'))
+        hash.update(flavor.encode('utf-8'))
+        hash.update(color.encode('utf-8'))
+        hash.update(points.encode('utf-8'))
         hash.update('version5')
         key = hash.hexdigest().lower()
 
@@ -396,7 +435,9 @@ class CardHandler(webapp2.RequestHandler):
         #jpg = None
         if jpg is None:
             if card == 'action':
-                jpg = actionCard(title, body, flavor)
+                jpg = actionCard(title, body, flavor, False)
+            elif card == 'action_codex':
+                jpg = actionCard(title, body, flavor, True)
             elif card == 'secret':
                 jpg = secretObjectiveCard(title, type, body)
             elif card == 'stage1':
@@ -410,12 +451,26 @@ class CardHandler(webapp2.RequestHandler):
             else:
                 self.response.status = 400 # bad request
                 self.response.status_message = 'Bad card type'
+                self.response.out.write('Bad card type "' + card + '"')
                 return
             memcache.add(key, jpg, 3600)
 
         self.response.headers['Content-Type'] = 'image/jpeg'
         self.response.out.write(jpg)
 
+class TestActionHandler(webapp2.RequestHandler):
+    def get(self):
+        title = 'Bribery'
+        body = 'After the speaker votes on an agenda: Spend any number of trade goods. For each trade good spent, cast 1 additional vote for any outcome.'
+        flavor = u'\u201CWe think that this initiative would spell disaster for the galaxy, not just the Creuss.\u201D Taivra said, quietly slipping Z\u2018eu an envelope. \u201CDon\u2019t you agree?\u201D'
+
+        jpg = actionCard(title.upper(), body, flavor, False)
+
+        self.response.headers['Content-Type'] = 'image/jpeg'
+        self.response.out.write(jpg)
+
 app = webapp2.WSGIApplication([
     ('/img', CardHandler),
+    ('/testaction', TestActionHandler),
+
 ], debug=True)
